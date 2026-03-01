24b4976ea053b95e2bd22d5627b6360b793a2fd4
leminhanh033 <leminhanh033@gmail.com>
2026-03-01 22:34:00 +0700

    Trang đăng nhập và đăng ký của ứng viên

--- src/app/(page)/(employee)/login/employee/page.tsx
+++ src/app/(page)/(employee)/login/employee/page.tsx
@@ -1,5 +1,5 @@
 import { Metadata } from "next";
-
+import {LoginEmployee} from "@/app/components/LoginEmployee/LoginEmployee"
 export const metadata: Metadata = {
     title: "Đăng nhập",
     description: "Trang đăng nhập của Ứng viên",
@@ -11,17 +11,7 @@ export default function Register() {
                 <div className="container flex justify-center">
                     <div className="w-[600px] rounded-[8px] p-[50px_20px]" style={{border: "1px solid #DEDEDE"}}>
                         <div className="text-center text-[20px] font-[700] text-[#000000]">Đăng nhập (Ứng viên)</div>
-                        <form action="" className="mt-[20px] flex flex-col gap-[15px] text-[14px] font-[500] text-[#000000]">
-                            <div>
-                                <label htmlFor="email">Email *</label>
-                                <input type="email" id="email" className="rounded-[4px] w-full p-[14px_20px]" style={{border: "1px solid #DEDEDE"}}/>
-                            </div>
-                            <div>
-                                <label htmlFor="pass">Mật khẩu *</label>
-                                <input type="password" id="pass" className="rounded-[4px] w-full p-[14px_20px]" style={{border: "1px solid #DEDEDE"}}/>
-                            </div>
-                            <button className="rounded-[4px] w-full bg-[#0088FF] p-[14px_20px] text-[16px] font-[700] text-[#FFFFFF] ">Đăng nhập</button>
-                        </form>
+                        <LoginEmployee/>
                     </div>
 
                 </div>
---
+++ src/app/components/LoginEmployee/LoginEmployee.tsx
@@ -0,0 +1,46 @@
+"use client";
+import JustValidate from 'just-validate';
+import { useEffect } from "react";
+
+export const LoginEmployee = () => {
+    useEffect(() => {
+        const validate = new JustValidate('#LoginEmployeeForm', {
+            errorFieldStyle: {},
+        });
+        validate
+            .addField('#email', [
+                {
+                    rule: 'required',
+                    errorMessage: "Vui lòng nhập email"
+                },
+                {
+                    rule: 'email',
+                    errorMessage: "Email không hợp lệ"
+                },
+
+            ])
+            .addField('#pass', [
+                {
+                    rule: 'required',
+                    errorMessage: "Vui lòng nhập mật khẩu"
+                },
+            ])
+            .onSuccess((event: any) => {
+                console.log(event.target.email.value);
+                console.log(event.target.pass.value);
+            })
+    }, [])
+    return <>
+        <form id="LoginEmployeeForm" action="" className="mt-[20px] flex flex-col gap-[15px] text-[14px] font-[500] text-[#000000]">
+            <div>
+                <label htmlFor="email">Email *</label>
+                <input type="email" id="email" className="rounded-[4px] w-full p-[14px_20px]" style={{ border: "1px solid #DEDEDE" }} />
+            </div>
+            <div>
+                <label htmlFor="pass">Mật khẩu *</label>
+                <input type="password" id="pass" className="rounded-[4px] w-full p-[14px_20px]" style={{ border: "1px solid #DEDEDE" }} />
+            </div>
+            <button className="rounded-[4px] w-full bg-[#0088FF] p-[14px_20px] text-[16px] font-[700] text-[#FFFFFF] ">Đăng nhập</button>
+        </form>
+    </>
+}
